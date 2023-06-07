6e171e34b90c154af2cf5b04142b918a0cded414
Lipeng Wong <huanglip0621@163.com>
2023-06-07 13:13:43 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
-const isProd = process.env.NODE_ENV === "production";
+//const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/websiteWithNextJs/" : "",
+  //assetPrefix: isProd ? "/websiteWithNextJs/" : "",
   images: {
     disableStaticImages: true,
   },
